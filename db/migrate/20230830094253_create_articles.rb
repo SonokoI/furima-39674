a7c0ce2308f9da5dca0cd2092a127a56375b7a34
SonokoI <141139126+SonokoI@users.noreply.github.com>
2023-09-01 17:57:07 +0900

    過剰記述のコメントアウト

--- db/migrate/20230830094253_create_articles.rb
+++ db/migrate/20230830094253_create_articles.rb
@@ -1,14 +1,14 @@
-class CreateArticles < ActiveRecord::Migration[7.0]
-  def change
-    create_table :articles do |t|
-      t.string     :title        , null: false
-      t.text       :text         , null: false
-      t.integer    :category_id     , null: false
-      t.integer    :condition_id     , null: false
-      t.integer    :shipping_cost_id     , null: false
-      t.integer    :shipping_area_id     , null: false
-      t.integer    :shipping_date_id     , null: false
-      t.timestamps
-    end
-  end
-end
+# class CreateArticles < ActiveRecord::Migration[7.0]
+#   def change
+#     create_table :articles do |t|
+#       t.string     :title        , null: false
+#       t.text       :text         , null: false
+#       t.integer    :category_id     , null: false
+#       t.integer    :condition_id     , null: false
+#       t.integer    :shipping_cost_id     , null: false
+#       t.integer    :shipping_area_id     , null: false
+#       t.integer    :shipping_date_id     , null: false
+#       t.timestamps
+#     end
+#   end
+# end
